38ac5bebea2ab30d121d29b4b59271691583d0f5
Víctor <Victorcanoje@gmail.com>
2016-05-29 16:21:58 +0200

    Creado SerializedSingleton con métodos para serializar y desserializar un singleton. Muy a tener en cuenta usar el método protected Object readResolve para que una vez que lea el obj. serializado lo reemplace en nuestro singleton

--- src/SerializedSingleton.java
+++ src/SerializedSingleton.java
@@ -1,10 +1,13 @@
-import java.io.Serializable;
+import org.omg.PortableInterceptor.INACTIVE;
+
+import java.io.*;
 
 /**
  * Created by Victor on 29/5/16.
  */
 public class SerializedSingleton implements Serializable {
     private static final long serialVersionUID = -423234234234234234L;
+    private static final String FILE_ANME = "serialized.bin";
     private static SerializedSingleton instance;
 
     private SerializedSingleton() {
@@ -16,5 +19,37 @@ public class SerializedSingleton implements Serializable {
             instance = new SerializedSingleton();
         }
         return instance;
+    }
+
+
+    public static void serializeMe() throws IOException {
+        if (instance != null) {
+            ObjectOutput out = new ObjectOutputStream(new FileOutputStream(FILE_ANME));
+
+            out.writeObject(instance);
+
+            out.flush();
+            out.close();
+        } else {
+            new Exception("SINGLETON IS NULL");
+        }
+    }
+
+    public static SerializedSingleton desSerializeMe() throws IOException, ClassNotFoundException {
+        ObjectInput in = new ObjectInputStream(new FileInputStream(FILE_ANME));
+
+        instance = (SerializedSingleton) in.readObject();
+        in.close();
+
+        return getInstance();
+    }
+
+
+    /*
+        Necesario para que una vez lea un obj serializado lo reemplazo por nuestra instancia. Si no lo hicieramos el singleton se perdería
+        y se crearía un nuevo obj.
+     */
+    protected Object readResolve() {
+        return getInstance();
     }
 }
